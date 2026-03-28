2c0caf39642527f0ff9bfd6a713006191647c7cd
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-03-28 11:11:43 +0530

    Add project files.

---
+++ API/Program.cs
@@ -0,0 +1,21 @@
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+         
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
